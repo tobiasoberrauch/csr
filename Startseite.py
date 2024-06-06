c059b597b2e39901463162569ae845ff974a3582
Tobias Oberrauch <tobias.oberrauch@audius.de>
2024-06-06 14:23:02 +0200

    add debug

--- Startseite.py
+++ Startseite.py
@@ -1,3 +1,15 @@
+import pytesseract
+import shutil
 import streamlit as st
 
-st.write('Startseite')
+pytesseract.pytesseract.tesseract_cmd = None
+
+# search for tesseract binary in path
+@st.cache_resource
+def find_tesseract_binary() -> str:
+    return shutil.which("tesseract")
+
+# set tesseract binary path
+pytesseract.pytesseract.tesseract_cmd = find_tesseract_binary()
+if not pytesseract.pytesseract.tesseract_cmd:
+    st.error("Tesseract binary not found in PATH. Please install Tesseract.")
