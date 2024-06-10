bfb5da1740fe6a938b997ec6c8df7d3ec039e609
Tobias Oberrauch <tobias.oberrauch@audius.de>
2024-06-10 15:18:37 +0200

    update

--- Startseite.py
+++ Startseite.py
@@ -3,6 +3,12 @@ import shutil
 
 import pytesseract
 import streamlit as st
+import streamlit.components.v1 as components
+
+# Include Google Analytics tracking code
+with open("index.html", "r") as f:
+    html_code = f.read()
+    components.html(html_code, height=0)
 
 logging.basicConfig(level=logging.INFO)
 
